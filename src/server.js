22c7303658dbeb2b76735a01a108df60bd2380e7
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-06-05 15:34:23 +0530

    setup express, middlewares and some utils

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 import connectDB from './db/index.js';
 import dotenv from 'dotenv';
+import app from './app.js';
 
 dotenv.config({
   path: './.env',
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT || 8000}`);
+    });
+
+    // check for errors
+    app.on('error', (err) => {
+      console.error('Express error:', err);
+    });
+  })
+  .catch((err) => console.log(`MongoDB connection error: ${err}`));
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+// const asyncHandler = (fn) => {()=>{}};
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message || 'Internal server error',
+    });
+  }
+};
+
+// using promises
+// const asyncHandler = (fn) => {
+//   return (req, res, next) => {
+//     Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+//   };
+// };
+
+export { asyncHandler };
