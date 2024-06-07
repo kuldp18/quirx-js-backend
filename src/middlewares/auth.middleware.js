e4aaf2cce9c54fcb5a5a8c2e066b56d31fd38246
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-06-07 14:50:26 +0530

    fix imports

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
-import { User } from '../models/user.model';
-import { asyncHandler } from '../utils/asyncHandler';
+import { User } from '../models/user.model.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
 import jwt from 'jsonwebtoken';
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
