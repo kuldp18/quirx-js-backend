9a1198783add92e2afc8fc017dd575e92687dfeb
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-06-09 09:04:23 +0530

    add remaining models

---
+++ src/models/tweet.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
+const tweetSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    content: {
+      type: String,
+      required: [true, 'Content is required'],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Tweet = mongoose.model('Tweet', tweetSchema);
