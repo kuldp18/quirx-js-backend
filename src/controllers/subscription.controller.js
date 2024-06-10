dbe6336caa243c3463a9b819731a4ac40120d801
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-06-10 17:49:40 +0530

    add toggle subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -6,8 +6,40 @@ import ApiResponse from '../utils/ApiResponse.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 const toggleSubscription = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new ApiError(404, 'Unauthorized user or user not found');
+  }
   const { channelId } = req.params;
-  // TODO: toggle subscription
+  if (!channelId) {
+    throw new ApiError(400, 'Channel ID is required');
+  }
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, 'Invalid Channel ID');
+  }
+
+  const existingSubscription = await Subscription.findOne({
+    subscriber: user._id,
+    channel: channelId,
+  });
+
+  if (existingSubscription) {
+    await Subscription.findByIdAndDelete(existingSubscription._id);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, null, 'Subscription removed successfully'));
+  }
+
+  const subscription = new Subscription({
+    subscriber: user._id,
+    channel: channelId,
+  });
+
+  await subscription.save();
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, subscription, 'Subscription successful'));
 });
 
 // controller to return subscriber list of a channel
