fc924c18204d038dea5cd65e5f6e54ad3c672c54
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-06-10 19:05:33 +0530

    fix subscription routes and add subscription controllers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -44,12 +44,76 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  // check if user is logged in
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new ApiError(404, 'Unauthorized user or user not found');
+  }
+  // get channel id from request params
   const { channelId } = req.params;
+  // validate channel id
+  if (!channelId) {
+    throw new ApiError(400, 'Channel ID is required');
+  }
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, 'Invalid Channel ID');
+  }
+  // find all subscribers of the channel
+  const subscribers = await Subscription.find({ channel: channelId });
+
+  if (!subscribers) {
+    throw new ApiError(
+      404,
+      'No subscribers found for this channel or error while fetching subscribers'
+    );
+  }
+  // return the list of subscribers
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, subscribers, 'Subscribers fetched successfully')
+    );
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
+  // check if user is logged in
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new ApiError(404, 'Unauthorized user or user not found');
+  }
+  // get subscriber id from request params
   const { subscriberId } = req.params;
+  // validate subscriber id
+  if (!subscriberId) {
+    throw new ApiError(400, 'Subscriber ID is required');
+  }
+
+  if (!isValidObjectId(subscriberId)) {
+    throw new ApiError(400, 'Invalid Subscriber ID');
+  }
+  // find all channels subscribed by the user
+  const subscribedChannels = await Subscription.find({
+    subscriber: subscriberId,
+  });
+
+  if (!subscribedChannels) {
+    throw new ApiError(
+      404,
+      'No subscribed channels found for this user or error while fetching subscribed channels'
+    );
+  }
+  // return the list of channels
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        subscribedChannels,
+        'Subscribed channels fetched successfully'
+      )
+    );
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
