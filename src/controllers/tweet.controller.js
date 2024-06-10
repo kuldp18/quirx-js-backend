82bcae33b275f7239361b0693ff764fb3a0a2490
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-06-10 16:07:34 +0530

    add create tweet and get user tweets controllers

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,11 +6,53 @@ import ApiResponse from '../utils/ApiResponse.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 const createTweet = asyncHandler(async (req, res) => {
-  //TODO: create tweet
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new ApiError(404, 'User not found or unauthorized');
+  }
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, 'Content is required');
+  }
+
+  const tweet = await Tweet.create({
+    owner: user._id,
+    content,
+  });
+
+  if (!tweet) {
+    throw new ApiError(500, 'Something went wrong while creating your tweet');
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, tweet, 'Tweet created successfully'));
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
-  // TODO: get user tweets
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new ApiError(404, 'Unauthorized user');
+  }
+
+  const { userId } = req.params;
+  if (!userId) {
+    throw new ApiError(400, 'User ID is required');
+  }
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, 'Invalid User ID');
+  }
+
+  const userTweets = await Tweet.find({ owner: userId });
+  if (!userTweets) {
+    throw new ApiError(404, 'User tweets not found');
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userTweets, 'User tweets fetched successfully'));
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
