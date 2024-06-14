d930200f93ab386eace0647054773710daedcaed
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-06-14 15:00:40 +0530

    add toggle video like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,10 +3,47 @@ import { Like } from '../models/like.model.js';
 import ApiError from '../utils/ApiError.js';
 import ApiResponse from '../utils/ApiResponse.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
+import { User } from '../models/user.model.js';
+import { Video } from '../models/video.model.js';
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new ApiError(404, 'User not found or unauthorized');
+  }
   const { videoId } = req.params;
-  //TODO: toggle like on video
+
+  if (!videoId) {
+    throw new ApiError(400, 'Invalid video id');
+  }
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, 'Invalid video id');
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, 'Video not found');
+  }
+
+  const like = await Like.findOne({ video: videoId, likedBy: user._id });
+
+  if (!like) {
+    const newLike = await Like.create({
+      video: videoId,
+      likedBy: user._id,
+    });
+    return res
+      .status(201)
+      .json(new ApiResponse(201, newLike, 'Video liked successfully'));
+  }
+
+  // delete like
+  await Like.findByIdAndDelete(like._id);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, 'Video like removed successfully'));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
