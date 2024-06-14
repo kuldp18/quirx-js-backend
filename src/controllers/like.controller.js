09ed601246f55d3074d72e02976f4c5530f7892c
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-06-14 15:08:23 +0530

    add toggle comment like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,6 +5,7 @@ import ApiResponse from '../utils/ApiResponse.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { User } from '../models/user.model.js';
 import { Video } from '../models/video.model.js';
+import { Comment } from '../models/comment.model.js';
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
@@ -47,8 +48,46 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new ApiError(404, 'User not found or unauthorized');
+  }
   const { commentId } = req.params;
-  //TODO: toggle like on comment
+  if (!commentId) {
+    throw new ApiError(400, 'Invalid comment id');
+  }
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, 'Invalid comment id');
+  }
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(404, 'Comment not found');
+  }
+
+  const commentLike = await Like.findOne({
+    comment: commentId,
+    likedBy: user._id,
+  });
+
+  if (!commentLike) {
+    const newCommentLike = await Like.create({
+      comment: commentId,
+      likedBy: user._id,
+    });
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, newCommentLike, 'Comment liked successfully'));
+  }
+
+  // delete comment like
+  await Like.findByIdAndDelete(commentLike._id);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, 'Comment like removed successfully'));
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
