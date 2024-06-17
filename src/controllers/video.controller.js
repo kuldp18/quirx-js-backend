4764cf6a07c49e822ea5dc6ce519cf25630879dd
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2024-06-18 00:00:47 +0530

    add get all user videos controller with pagination and queries

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -21,8 +21,101 @@ const removeLocalFiles = (files) => {
 };
 
 const getAllVideos = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    throw new ApiError(404, 'User not found or unauthorized');
+  }
+
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
-  //TODO: get all videos based on query, sort, pagination
+
+  let pipeline = [];
+
+  if (query) {
+    pipeline.push({
+      $search: {
+        index: 'search-videos',
+        text: {
+          query: query,
+          path: ['title', 'description'],
+        },
+      },
+    });
+  }
+
+  if (!userId) {
+    throw new ApiError(400, 'Please provide a user id');
+  }
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, 'Invalid user id');
+  }
+
+  pipeline.push({
+    $match: {
+      owner: new mongoose.Types.ObjectId(userId),
+    },
+  });
+
+  // get only public videos
+  pipeline.push({
+    $match: {
+      isPublished: true,
+    },
+  });
+
+  //sortBy can be views, createdAt, duration
+  //sortType can be ascending(-1) or descending(1)
+
+  if (sortBy && sortType) {
+    pipeline.push({
+      $sort: {
+        [sortBy]: sortType === 'asc' ? 1 : -1,
+      },
+    });
+  } else {
+    pipeline.push({ $sort: { createdAt: -1 } });
+  }
+
+  // get owner details
+  pipeline.push(
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'owner',
+        foreignField: '_id',
+        as: 'ownerDetails',
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              'avatar.url': 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: '$ownerDetails',
+    }
+  );
+
+  const videoAggregate = Video.aggregate(pipeline);
+
+  const options = {
+    page: parseInt(page, 10),
+    limit: parseInt(limit, 10),
+  };
+
+  const videos = await Video.aggregatePaginate(videoAggregate, options);
+
+  if (!videos) {
+    throw new ApiError(404, 'No videos found');
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, 'Videos retrieved successfully'));
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
